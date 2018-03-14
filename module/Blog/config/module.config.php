44104f0764260c7dbc14a8e3a4ea7a9d2eac3623
Dmitriy Knysh <dm32.knysh@gmail.com>
2018-03-14 17:04:41 +0200

    Doctrine #1

---
+++ module/Blog/config/module.config.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+return array(
+    'controllers' => array(
+        'invokables' => array(
+            'Blog\Controller\Index' => 'Blog\Controller\IndexController',
+        ),
+    ),
+    'router' => array(
+        'routes' => array(
+            'blog' => array(
+                'type'    => 'segment',
+                'options' => array(
+                    'route'    => '[/:action][/:id]',
+                    'constraints' => array(
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'     => '[0-9]+',
+                    ),
+                    'defaults' => array(
+                        'controller' => 'Blog\Controller\Index',
+                        'action'     => 'index',
+                    ),
+                ),
+            ),
+        ),
+    ),
+    'view_manager' => array(
+        'template_path_stack' => array(
+            'album' => __DIR__ . '/../view',
+        ),
+    ),
+);
